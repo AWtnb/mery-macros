dc4b7214e49460e0d00384fde70056e8750b312e
awtnb <arikaebanataw@gmail.com>
2024-10-09 18:14:00 +0900

    update

---
+++ Macros/ToMdList.js
@@ -0,0 +1,34 @@
+// ------------------------------------------------------------
+// 各行頭にmarkdownのリスト記号を入れる
+// ------------------------------------------------------------
+
+function main() {
+  const symbol = "- ";
+  if (document.selection.Mode != meModeStream) {
+    return;
+  }
+  if (document.selection.isEmpty) {
+    document.selection.text = symbol;
+    return;
+  }
+
+  const sel = document.selection;
+  const ty = sel.GetTopPointY(mePosLogical);
+  const by = sel.GetBottomPointY(mePosLogical);
+
+  document.selection.SetActivePoint(mePosLogical, 1, ty, false);
+  document.selection.SetActivePoint(mePosLogical, 1, by, true);
+  document.selection.EndOfLine(true);
+
+  const t = document.selection.text;
+
+  const lines = t.split("\n");
+  const stack = [];
+  for (let i = 0; i < lines.length; i++) {
+    const line = lines[i];
+    stack.push(symbol + line);
+  }
+  document.selection.text = stack.join("\n");
+}
+
+main();
