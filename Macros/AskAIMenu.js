86ff12fd24ee78c842e76e5db9fa173e9dd0d1d8
awtnb <arikaebanataw@gmail.com>
2024-12-12 11:16:16 +0900

    add macro

---
+++ Macros/AskAIMenu.js
@@ -0,0 +1,47 @@
+function toList(s) {
+  const ret = [];
+  const ss = s.split("\n");
+  for (let i = 0; i < ss.length; i++) {
+    const l = ss[i];
+    if (0 < l.trim().length) {
+      ret.push("- " + l);
+    }
+  }
+  return ret.join("\n");
+}
+
+function main() {
+  if (document.selection.Mode != meModeStream) {
+    return;
+  }
+
+  if (document.selection.IsEmpty) {
+    document.selection.SelectAll();
+  }
+
+  const menuArr = [
+    ["Gemini", "メール文面", "https://gemini.google.com/app", "以下の内容を伝えるビジネスメールの文面を書いてください。\n------\n{}"],
+    ["ChatGPT", "メール文面", "https://chat.openai.com/chat", "以下の内容を伝えるビジネスメールの文面を書いてください。\n------\n{}"],
+  ];
+
+  const menu = CreatePopupMenu();
+  for (let i = 0; i < menuArr.length; i++) {
+    const m = menuArr[i];
+    menu.Add(m[0] + "\t" + m[1], i + 1);
+  }
+
+  const idx = menu.Track(0);
+  if (idx < 1) {
+    return;
+  }
+
+  const selected = menuArr[idx - 1];
+
+  const t = toList(document.selection.Text);
+  ClipboardData.SetData(selected[3].replace("{}", t));
+
+  const shell = new ActiveXObject("WScript.Shell");
+  shell.Run(selected[2]);
+}
+
+main();
