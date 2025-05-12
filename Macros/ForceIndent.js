58aedc321f64063b5817aad45f19084dd7e314a5
awtnb <arikaebanataw@gmail.com>
2025-05-12 21:09:06 +0900

    update indent

--- Macros/ForceIndent.js
+++ Macros/ForceIndent.js
@@ -1,14 +1,32 @@
+function getLogicalLine(y) {
+  const lines = document.text.split(/\r?\n/);
+  return lines[y - 1];
+}
+
 function main() {
   var sel = Document.Selection;
-  if (sel.Text.indexOf("\n") < 0) {
-    var x = sel.GetActivePointX(mePosLogical);
-    var y = sel.GetActivePointY(mePosLogical);
-    sel.SelectLine();
-    var n = sel.Text.length;
+
+  if (document.selection.Mode != meModeStream) {
     sel.Indent();
-    sel.SetActivePoint(mePosLogical, x - (n - sel.Text.length), y);
-  } else {
-    sel.Indent();
+    return;
   }
+
+  const anchor = sel.GetAnchorPos();
+  const activeX = sel.GetActivePointX(mePosLogical);
+  const activeY = sel.GetActivePointY(mePosLogical);
+
+  const lineBefore = getLogicalLine(activeY);
+  if (lineBefore.length < 1) {
+    sel.text = "    " + sel.text;
+    return;
+  }
+
+  sel.Indent();
+  const lineAfter = getLogicalLine(activeY);
+
+  const delta = lineAfter.length - lineBefore.length;
+  sel.SetAnchorPos(anchor + delta);
+  sel.SetActivePoint(mePosLogical, activeX + delta, activeY, true);
 }
+
 main();
--- Macros/ForceOutdent.js
+++ Macros/ForceOutdent.js
@@ -1,14 +1,31 @@
+function getLogicalLine(y) {
+  const lines = document.text.split(/\r?\n/);
+  return lines[y - 1];
+}
+
 function main() {
   var sel = Document.Selection;
-  if (sel.Text.indexOf("\n") < 0) {
-    var x = sel.GetActivePointX(mePosLogical);
-    var y = sel.GetActivePointY(mePosLogical);
-    sel.SelectLine();
-    var n = sel.Text.length;
+
+  if (document.selection.Mode != meModeStream) {
     sel.UnIndent();
-    sel.SetActivePoint(mePosLogical, x - (n - sel.Text.length), y);
-  } else {
-    sel.UnIndent();
+    return;
   }
+
+  const anchor = sel.GetAnchorPos();
+  const activeX = sel.GetActivePointX(mePosLogical);
+  const activeY = sel.GetActivePointY(mePosLogical);
+
+  const lineBefore = getLogicalLine(activeY);
+  if (lineBefore.length < 1 || lineBefore.replace(/\s+/, "") == lineBefore) {
+    return;
+  }
+
+  sel.UnIndent();
+  const lineAfter = getLogicalLine(activeY);
+
+  const delta = lineBefore.length - lineAfter.length;
+  sel.SetAnchorPos(anchor - delta);
+  sel.SetActivePoint(mePosLogical, activeX - delta, activeY, true);
 }
+
 main();
