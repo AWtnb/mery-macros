9b380367ea58e531578d9c4de46188be1461d7d2
awtnb <arikaebanataw@gmail.com>
2025-05-13 09:55:17 +0900

    fix

--- Macros/OpenDraft.js
+++ Macros/OpenDraft.js
@@ -2,6 +2,9 @@ function main() {
   const wshShell = new ActiveXObject("WScript.Shell");
   const fso = new ActiveXObject("Scripting.FileSystemObject");
   const draft = wshShell.ExpandEnvironmentStrings("%USERPROFILE%\\Personal\\draft.txt");
+  if (draft == document.fullname) {
+    return;
+  }
   if (fso.FileExists(draft)) {
     wshShell.Run(draft);
   } else {
