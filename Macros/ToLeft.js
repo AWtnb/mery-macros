07eb75cf170b189620fc15f7ca912000ca83c029
awtnb <arikaebanataw@gmail.com>
2025-05-16 12:06:16 +0900

    fix

--- Macros/ToLeft.js
+++ Macros/ToLeft.js
@@ -3,21 +3,20 @@
 // 選択中であれば常に選択範囲の左に。
 // ------------------------------------------------------------
 
-function main () {
+function main() {
   if (document.selection.IsEmpty) {
     document.selection.CharLeft();
     return;
   }
   if (document.selection.Mode != meModeStream) {
-    document.selection.Collapse(meCollapseStart);
+    const opt = document.selection.GetAnchorPos(0) < document.selection.GetActivePos(0) ? meCollapseStart : meCollapseEnd;
+    document.selection.Collapse(opt);
     return;
   }
 
-  // Redraw = false;
   const x = document.selection.GetTopPointX(mePosLogical);
   const y = document.selection.GetTopPointY(mePosLogical);
-  document.selection.SetActivePoint(mePosLogical, x, y)
-  // Redraw = true;
+  document.selection.SetActivePoint(mePosLogical, x, y);
 }
 
 main();
--- Macros/ToRight.js
+++ Macros/ToRight.js
@@ -3,21 +3,19 @@
 // 選択中であれば常に選択範囲の右に。
 // ------------------------------------------------------------
 
-function main () {
+function main() {
   if (document.selection.IsEmpty) {
     document.selection.CharRight();
     return;
   }
   if (document.selection.Mode != meModeStream) {
-    document.selection.Collapse(meCollapseEnd);
+    const opt = document.selection.GetAnchorPos(0) < document.selection.GetActivePos(0) ? meCollapseEnd : meCollapseStart;
+    document.selection.Collapse(opt);
     return;
   }
-
-  // Redraw = false;
   const x = document.selection.GetBottomPointX(mePosLogical);
   const y = document.selection.GetBottomPointY(mePosLogical);
-  document.selection.SetActivePoint(mePosLogical, x, y)
-  // Redraw = true;
+  document.selection.SetActivePoint(mePosLogical, x, y);
 }
 
 main();
