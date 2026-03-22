109f7a5c3702a2b4ce70b1dc3bfa767213167283
awtnb <46807238+AWtnb@users.noreply.github.com>
2026-03-22 22:08:34 +0900

    add smooth csv macro

---
+++ Macros/OpenSmoothCSV.js
@@ -0,0 +1,13 @@
+function main() {
+  const wshShell = new ActiveXObject("WScript.Shell");
+  const fso = new ActiveXObject("Scripting.FileSystemObject");
+  const smoothCSV = "C:\\Program Files\\SmoothCSV\\smoothcsv-app.exe"
+  const path = document.fullname
+  if (fso.FileExists(smoothCSV)) {
+    wshShell.Run('"' + smoothCSV + '" "' + path + '"');
+  } else {
+    alert("'" + smoothCSV + "' が存在しません！");
+  }
+}
+
+main();
