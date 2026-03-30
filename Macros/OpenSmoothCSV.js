2aa5e10b90dc38fc42a444b2f6f694d2f3d1da59
AWtnb <46807238+AWtnb@users.noreply.github.com>
2026-03-30 09:47:37 +0900

    close after open csv

--- Macros/OpenSmoothCSV.js
+++ Macros/OpenSmoothCSV.js
@@ -1,10 +1,11 @@
 function main() {
   const wshShell = new ActiveXObject("WScript.Shell");
   const fso = new ActiveXObject("Scripting.FileSystemObject");
-  const smoothCSV = "C:\\Program Files\\SmoothCSV\\smoothcsv-app.exe"
-  const path = document.fullname
+  const smoothCSV = "C:\\Program Files\\SmoothCSV\\smoothcsv-app.exe";
+  const path = document.fullname;
   if (fso.FileExists(smoothCSV)) {
     wshShell.Run('"' + smoothCSV + '" "' + path + '"');
+    window.close();
   } else {
     alert("'" + smoothCSV + "' が存在しません！");
   }
